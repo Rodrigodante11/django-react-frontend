e4ebebf3eaca3e4c64439b829f590af2c5f7c7d1
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2023-03-10 14:57:18 -0300

    add token autorization on front to consume API Django

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,12 +1,35 @@
 import React from 'react'
 import ListComponent from './ListComponent';
 
-export default function UserList(){
+export default class UserList extends React.Component{
+    state = {
+        list: null,
+        loading: false
+    }
 
-    return (
-        <div>
+    async componentDidMount(){
+        const config = {
+
+            headers:{
+                'Content-type': 'application/json'
+            }
+        }
+        config.headers['Authorization'] = "Token df19b57fdfffc473417b2b638fe9c41401a59a97"
+        var url = 'http://localhost:8000/list/';
+
+        const response = await fetch(url, config);
+        const data = await response.json();
+        console.log(data);
+        this.setState({list: data , loading: false});
+
+    }
+    render() {
+        return (
+            <div>
             <ListComponent listName={'Minha Lista'} />
             <ListComponent listName={'Minha Lista 2'} />
         </div>
-    );
+        )
+        
+    };
 }
